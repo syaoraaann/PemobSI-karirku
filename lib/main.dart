f73a8afd31eb573d72c6573173ddbe43665c6c2e
syaoraaann <kevinptm1504@gmail.com>
2024-04-11 16:55:27 +0800

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_super_parameters
+// ignore_for_file: use_super_parameters, use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:karirku/screens/joblist_screen.dart';
@@ -6,9 +6,183 @@ import 'package:karirku/screens/news_screen.dart';
 import 'package:karirku/screens/routes/SecondScreen/second_screen.dart';
 import 'package:karirku/screens/community_screen.dart';
 import 'package:karirku/screens/profile_screen.dart';
+import 'package:animate_do/animate_do.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 400,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: -40,
+                    height: 400,
+                    width: width,
+                    child: FadeInUp(
+                        duration: Duration(seconds: 1),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/background.png'),
+                                  fit: BoxFit.fill)),
+                        )),
+                  ),
+                  Positioned(
+                    height: 400,
+                    width: width + 20,
+                    child: FadeInUp(
+                        duration: Duration(milliseconds: 1000),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/background-2.png'),
+                                  fit: BoxFit.fill)),
+                        )),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1500),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Color.fromRGBO(49, 39, 79, 1),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1700),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            border: Border.all(
+                                color: Color.fromRGBO(196, 135, 198, .3)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(196, 135, 198, .3),
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              )
+                            ]),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Color.fromRGBO(
+                                              196, 135, 198, .3)))),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Username",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey.shade700)),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey.shade700)),
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1700),
+                      child: Center(
+                          child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                    color: Color.fromRGBO(196, 135, 198, 1)),
+                              )))),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1900),
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyApp(),
+                            ),
+                          );
+                        },
+                        color: Color.fromRGBO(49, 39, 79, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 2000),
+                      child: Center(
+                          child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Create Account",
+                                style: TextStyle(
+                                    color: Color.fromRGBO(49, 39, 79, .6)),
+                              )))),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +197,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'Home Screen'),
       routes: {
+        '/job_list': (context) => const JobList(),
         '/second-screen': (context) => const SecondScreen(),
         '/news-screen': (context) => const NewsScreen()
       },
